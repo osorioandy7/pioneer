fcb1e2e8be568b749f7acd3d83420c7775ceca83
takeaderivative <demouser@MacBook-Air.local>
2017-07-24 12:17:29 -0700

    forum

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import webapp2
 import os
 import jinja2
 from forum_database import Poster
+import logging
+
 env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
@@ -17,10 +19,14 @@ class ForumHandler(webapp2.RequestHandler):
 class ForumNewQuestionHandler(webapp2.RequestHandler):
     def get(self):
         template2b = env.get_template("static_folder/forum_new_question.html")
-        user_name1 = Poster(user_name = "Thomas", email_address = "thomas@usa.gov", post_content = "yayayayayyayy")
-        self.response.out.write(template2b.render())
+        thomas = Poster(user_name = "Thomas", email_address = "thomas@usa.gov", post_content = "yayayayayyayy")
+        key_thomas = thomas.put()
+        thomas_name = key_thomas.get().user_name
+        self.response.out.write(template2b.render(name = thomas_name))
     def post(self):
-        self.response.write("You have successfully submitted your post!")
+        template2a = env.get_template("static_folder/mapspage.html")
+        self.response.out.write(template2a.render())
+        self.response.write("You have successfully submitted your post!" )
 
 class MapHandler(webapp2.RequestHandler):
     def get(self):
