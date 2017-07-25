d9f504e233ebf98e81822bd7fa9a5ba9a8101c29
takeaderivative <rebeccazeng13@gmail.com>
2017-07-25 09:56:13 -0700

    forum works to datatbase

--- main.py
+++ main.py
@@ -9,40 +9,26 @@ env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         template = env.get_template("static_folder/main.html")
-        self.response.out.write(template.render(var1 = "hi"))
+        self.response.out.write(template.render())
 
 class ForumHandler(webapp2.RequestHandler):
+    # to show the page and template
     def get(self):
         template2a = env.get_template("static_folder/forum.html")
         post_content = {}
         self.response.out.write(template2a.render())
-
+    # to get responses from the user input boxes
     def post(self):
         result_template = env.get_template("static_folder/forum.html")
         template_variables = {'user_name' : self.request.get('user_name'),
                               'email_address': self.request.get('email_address'),
-                              'post_content': self.request.get('post_content')
                             }
-        Poster(user_name = template_variables['user_name'], email_address= template_variables['email_address']).put()
+        user1 = Poster(user_name = template_variables['user_name'], email_address= template_variables['email_address'])
+        post_content_key = user1.put()
 
     #thePosters = Poster(user_name = template_variables['user_name'],  )
-
-        thePosts = Posts(post_content = "fdoisjfiojfdissjdkkshdfjlodsf")
-        self.response.out.write(template2a.render(post_content))
-#user_name = ndb.StringProperty()
-#email_address = ndb.StringProperty()
-#post_content = ndb.StringProperty()
-# class ForumNewQuestionHandler(webapp2.RequestHandler):
-#     def get(self):
-#         template2b = env.get_template("static_folder/forum_new_question.html")
-#         thomas = Poster(user_name = "Thomas", email_address = "thomas@usa.gov", post_content = "yayayayayyayy")
-#         key_thomas = thomas.put()
-#         thomas_name = key_thomas.get().user_name
-#         self.response.out.write(template2b.render(name = thomas_name))
-    # def post(self):
-    #     template2a = env.get_template("static_folder/mapspage.html")
-    #     self.response.out.write(template2a.render())
-    #     self.response.write("You have successfully submitted your post!" )
+    #thePosts = Posts(post_content = "fdoisjfiojfdissjdkkshdfjlodsf")
+        self.response.out.write(result_template.render(post_content_key))
 
 class MapHandler(webapp2.RequestHandler):
     def get(self):
