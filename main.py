ff74e0c3563ce39e4396a18c36f8b03a9a32ebce
takeaderivative <rebeccazeng13@gmail.com>
2017-07-26 11:24:04 -0700

    new forum

--- main.py
+++ main.py
@@ -20,8 +20,9 @@ class CssiUser(ndb.Model):
     preferences, etc, you need to create a Datastore model like this
     example).
     """
-    first_name = ndb.StringProperty()
+    user_name = ndb.StringProperty()
     last_name = ndb.StringProperty()
+    email = ndb.StringProperty()
 
 
 class MainHandler(webapp2.RequestHandler):
@@ -34,16 +35,75 @@ class MainHandler(webapp2.RequestHandler):
 class ForumHandler(webapp2.RequestHandler):
     # to show the page and template
 
+    def get(self):
+        user = users.get_current_user()
+        # If the user is logged in...
+
+        if user:
+            email_address = user.nickname()
+            # We could also do a standard query, but ID is special and it
+            # has a special method to retrieve entries using it.
+
+            # If the user has previously been to our site, we greet them!
+            cssi_user = CssiUser.get_by_id(user.user_id())
+            signout_link_html = '<a href="%s">sign out</a>' % (
+                users.create_logout_url('/forum'))
+            # if CssiUser.query(CssiUser.email == user.email()).get() == False:
+            #     cssi_user_object = CssiUser(email=user.email(),user_name=user.nickname()).put()
+
+            logging.info("Viewpage1")
+            self.viewPage()
+            logging.info("Viewpage3")
+            # If the user hasn't been to our site, we ask them to sign up
+        # Otherwise, the user isn't logged in!
+        else:
+            self.response.write('''
+                Please log in to use our site! <br>
+                <a href="%s">Sign in</a>''' % (
+                    users.create_login_url('/forum')))
+
     def getForum(self):
+        user = users.get_current_user()
         template2 = env.get_template("static_folder/forum.html")
         post_content = {}
         self.response.out.write(template2.render())
 
-        template_variables = {'user_name' : self.request.get('user_name'),
-                              'email_address': self.request.get('email_address'),
-                              'post_text': self.request.get('post_text')
+        template_variables = {
+                        'user_name' : self.request.get('user_name'),
+                        'email_address': user.email(),
+                        'post_text': self.request.get('post_text')
                             }
-        user1 = Poster(user_name = template_variables['user_name'], email_address= template_variables['email_address'], post_text = template_variables['post_text'])
+        user1 = Poster(user_name = template_variables['user_name'], email_address= template_variables['email_address'], post_text = template_variables['post_text']).put()
+
+
+        user1_query = Poster.query()
+        all_results = user1_query.fetch()
+
+        signout_link_html = '<a href="%s">sign out</a>' % (
+            users.create_logout_url('/forum'))
+        self.response.out.write(signout_link_html)
+        for result in all_results:
+            #self.response.out.write(i)
+            self.response.out.write("<br>" + result.user_name + "<br>" + result.email_address + "<br>" + result.post_text)
+            logging.info('Hello, doing some lOOOOOOOO!')        
+
+        logging.info('Hello, doing some logging!')
+
+
+    def post(self):
+        self.getForum()
+    def viewPage(self):
+        user = users.get_current_user()
+        template2 = env.get_template("static_folder/forum.html")
+        post_content = {}
+        self.response.out.write(template2.render())
+
+        template_variables = {
+                        'user_name' : self.request.get('user_name'),
+                        'email_address': user.email(),
+                        'post_text': self.request.get('post_text')
+                            }
+        #user1 = Poster(user_name = template_variables['user_name'], email_address= template_variables['email_address'], post_text = template_variables['post_text']).put()
 
 
         user1_query = Poster.query()
@@ -59,48 +119,8 @@ class ForumHandler(webapp2.RequestHandler):
         for result in all_results:
             #self.response.out.write(i)
             self.response.out.write("<br>" + result.user_name + "<br>" + result.email_address + "<br>" + result.post_text)
-            logging.info('Hello, doing some loOPING!')
+            logging.info('Hello, doing some lOOOOOOOO!')
 
-
-
-
-
-    def get(self):
-        user = users.get_current_user()
-        # If the user is logged in...
-
-        if user:
-            email_address = user.nickname()
-            # We could also do a standard query, but ID is special and it
-            # has a special method to retrieve entries using it.
-
-            # If the user has previously been to our site, we greet them!
-            cssi_user = CssiUser.get_by_id(user.user_id())
-            signout_link_html = '<a href="%s">sign out</a>' % (
-                users.create_logout_url('/forum'))
-
-            if cssi_user:
-
-                self.getForum()
-            # If the user hasn't been to our site, we ask them to sign up
-            else:
-                self.response.write('''
-                    Welcome to our site, %s!  Please sign up! <br>
-                    <form method="post" action="/forum">
-                    <input type="text" name="first_name">
-                    <input type="text" name="last_name">
-                    <input type="submit">
-                    </form><br> %s <br>
-                    ''' % (email_address, signout_link_html))
-        # Otherwise, the user isn't logged in!
-        else:
-            self.response.write('''
-                Please log in to use our site! <br>
-                <a href="%s">Sign in</a>''' % (
-                    users.create_login_url('/forum')))
-
-    def post(self):
-        self.getForum()
 class MapHandler(webapp2.RequestHandler):
     def get(self):
         template3 = env.get_template("static_folder/mapspage.html")
