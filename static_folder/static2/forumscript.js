009f6a57ee03058567672b1aa6711a67c3853bf0
takeaderivative <rebeccazeng13@gmail.com>
2017-07-26 22:15:51 -0700

    forums with filters

--- static_folder/static2/forumscript.js
+++ static_folder/static2/forumscript.js
@@ -1,33 +1,35 @@
 console.log("hihihiihihi");
 
-//
-// function applyFilterClick() {
-//   $('#apply_button').click(applyFilter);
-//   console.log("CONSOLEELELELELELLELE")
-// }
-//
-// $(document).ready(
-//   applyFilterClick,
-//   applyFilter
-// );
+
+function applyFilterClick() {
+  $('#apply_button').click(applyFilter);
+  console.log("CONSOLEELELELELELLELE")
+}
+
+$(document).ready(
+  applyFilterClick
+);
 
 function applyFilter() {
+  // still doesn't account for clicking both!!
   $('.uci').show();
   $('.ucsd').show();
-  console.log("hehhehehehsxfdsf")
+  console.log("hehhehehehsxfdsf");
 
   if ($('#uci').is(':checked')){
     $('.ucsd').hide();
+    $('.uci').show();
     console.log("U C I");
   }
   else if ($('#ucsd').is(':checked')) {
     $('.uci').hide();
+    $('.ucsd').show();
     console.log("U C S D");
   }
   else {
-    $('.uci').show();
-    $('.ucsd').show();
-    console.log("hehhehehehsxfdsf")
-  }
+    // $('.uci').show();
+    // $('.ucsd').show();
+    // console.log("hehhehehehsxfdsf");
+    }
 
 }
