75f25ddf8ffa42ae5292ef9fd30f3c9df39c8233
takeaderivative <rebeccazeng13@gmail.com>
2017-07-27 09:22:43 -0700

    forums filters work now

--- static_folder/static2/forumscript.js
+++ static_folder/static2/forumscript.js
@@ -8,101 +8,61 @@ $(document).ready(
   applyFilterClick
 );
 
+$('.mit').show();
+$('.uci').show();
+$('.ucsd').show();
+$('.northridge').show();
+$('.ucsc').show();
+$('.ucb').show();
+$('.cppomona').show();
+$('.cpslo').show();
+
 function applyFilter() {
   // still doesn't account for clicking both!!
-  $('.mit').show();
-  $('.uci').show();
-  $('.ucsd').show();
-  $('.northridge').show();
-  $('.ucsc').show();
-  $('.ucb').show();
-  $('.cppomona').show();
-  $('.cpslo').show();
-
+  $('.mit').hide();
+  $('.uci').hide();
+  $('.ucsd').hide();
+  $('.northridge').hide();
+  $('.ucsc').hide();
+  $('.ucb').hide();
+  $('.cppomona').hide();
+  $('.cpslo').hide();
 
   if ($('#uci').is(':checked')){
-    $('.mit').hide();
     $('.uci').show();
-    $('.ucsd').hide();
-    $('.northridge').hide();
-    $('.ucsc').hide();
-    $('.ucb').hide();
-    $('.cppomona').hide();
-    $('.cpslo').hide();
+
   }
-  else if ($('#ucsd').is(':checked')) {
-    $('.mit').hide();
-    $('.uci').hide();
+  if ($('#ucsd').is(':checked')) {
+
     $('.ucsd').show();
-    $('.northridge').hide();
-    $('.ucsc').hide();
-    $('.ucb').hide();
-    $('.cppomona').hide();
-    $('.cpslo').hide();
+
   }
-  else if ($('#mit').is(':checked')) {
+  if ($('#mit').is(':checked')) {
     $('.mit').show();
-    $('.uci').hide();
-    $('.ucsd').hide();
-    $('.northridge').hide();
-    $('.ucsc').hide();
-    $('.ucb').hide();
-    $('.cppomona').hide();
-    $('.cpslo').hide();
   }
 
-  else if ($('#ucb').is(':checked')) {
-    $('.mit').hide();
-    $('.uci').hide();
-    $('.ucsd').hide();
-    $('.northridge').hide();
-    $('.ucsc').hide();
+  if ($('#ucb').is(':checked')) {
     $('.ucb').show();
-    $('.cppomona').hide();
-    $('.cpslo').hide();
+
   }
 
-  else if ($('#northridge').is(':checked')) {
-    $('.mit').hide();
-    $('.uci').hide();
-    $('.ucsd').hide();
+  if ($('#northridge').is(':checked')) {
+
     $('.northridge').show();
-    $('.ucsc').hide();
-    $('.ucb').hide();
-    $('.cppomona').hide();
-    $('.cpslo').hide();
+
   }
 
-  else if ($('#ucsc').is(':checked')) {
-    $('.mit').hide();
-    $('.uci').hide();
-    $('.ucsd').hide();
-    $('.northridge').hide();
+  if ($('#ucsc').is(':checked')) {
     $('.ucsc').show();
-    $('.ucb').hide();
-    $('.cppomona').hide();
-    $('.cpslo').hide();
+
   }
 
-  else if ($('#cppomona').is(':checked')) {
-    $('.mit').hide();
-    $('.uci').hide();
-    $('.ucsd').hide();
-    $('.northridge').hide();
-    $('.ucsc').hide();
-    $('.ucb').hide();
+  if ($('#cppomona').is(':checked')) {
     $('.cppomona').show();
-    $('.cpslo').hide();
+
   }
 
-  else if ($('#cpslo').is(':checked')) {
-    $('.mit').hide();
-    $('.uci').hide();
-    $('.ucsd').hide();
-    $('.northridge').hide();
-    $('.ucsc').hide();
-    $('.ucb').hide();
-    $('.cppomona').hide();
+  if ($('#cpslo').is(':checked')) {
     $('.cpslo').show();
   }
 
