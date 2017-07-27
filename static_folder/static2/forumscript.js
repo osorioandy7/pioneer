eb269482ba41ea00b283ae825dda9e6646b1137b
takeaderivative <rebeccazeng13@gmail.com>
2017-07-26 18:06:35 -0700

    forum more checkboxes

--- static_folder/static2/forumscript.js
+++ static_folder/static2/forumscript.js
@@ -1,19 +1,33 @@
 console.log("hihihiihihi");
 
-
-function applyFilterClick() {
-  $('#apply_button').click();
-}
-
-$(document).ready(
-  applyFilter
-);
+//
+// function applyFilterClick() {
+//   $('#apply_button').click(applyFilter);
+//   console.log("CONSOLEELELELELELLELE")
+// }
+//
+// $(document).ready(
+//   applyFilterClick,
+//   applyFilter
+// );
 
 function applyFilter() {
+  $('.uci').show();
+  $('.ucsd').show();
+  console.log("hehhehehehsxfdsf")
 
+  if ($('#uci').is(':checked')){
+    $('.ucsd').hide();
+    console.log("U C I");
+  }
+  else if ($('#ucsd').is(':checked')) {
+    $('.uci').hide();
+    console.log("U C S D");
+  }
+  else {
+    $('.uci').show();
+    $('.ucsd').show();
+    console.log("hehhehehehsxfdsf")
+  }
 
 }
-
-$(".uci").
-
-$(".ucsd").
